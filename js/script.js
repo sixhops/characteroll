c2020a2a1642fa08851c9ecae665b676e72f1d9a
Steve Peters <sixhops@gmail.com>
2017-05-12 21:05:47 -0700

    Adding JS and updating HTML

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+function rollAllParams() {
+  document.getElementById("pitchslider").value = Math.round(Math.random() * 10)
+  document.getElementById("sizespeedcontrol");
+  document.getElementById("articulatorselect");
+}
